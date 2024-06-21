c83bc7262be91cfa969a4c27870e0e5c4f6afed9
Jo Youngjae <106523863+youngjae0401@users.noreply.github.com>
2024-06-21 15:45:36 +0900

    update screenshot -> pdf convert file

--- screenshot_convert_to_pdf.py
+++ screenshot_convert_to_pdf.py
@@ -1,3 +1,4 @@
+# 라이브러리 설치
 # pip install pyautogui pillow reportlab pyinstaller PyQt5
 
 import sys
@@ -6,13 +7,13 @@ import pyautogui
 from PIL import Image
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QRadioButton, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QButtonGroup
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QRadioButton, QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QButtonGroup, QDesktopWidget
 
-class ScreenshotApp(QWidget):
+class ConvertPdfApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.count_down = 5
-        self.user_delay = 3
+        self.count_down = 5 # 처음 타이머 시간
+        self.user_delay = 3 # 사용자 마우스 이동 대기시간
         self.top_left_x = 0
         self.top_left_y = 0
         self.bottom_right_x = 0
@@ -24,15 +25,24 @@ class ScreenshotApp(QWidget):
     def initUI(self):
         # 좌상단 좌표 설정 버튼
         self.top_left_label = QLabel('좌상단 좌표:')
+        self.left_x_input = QLineEdit(self)
+        self.left_y_input = QLineEdit(self)
+        self.left_x_input.setPlaceholderText("x 좌표")
+        self.left_y_input.setPlaceholderText("y 좌표")
         self.top_left_button = QPushButton('좌상단 좌표 설정', self)
-        self.top_left_button.clicked.connect(self.set_top_left_coordinates)
+        self.top_left_button.clicked.connect(self.set_left_top_coordinates)
 
         # 우하단 좌표 설정 버튼
         self.bottom_right_label = QLabel('우하단 좌표:')
+        self.right_x_input = QLineEdit(self)
+        self.right_y_input = QLineEdit(self)
+        self.right_x_input.setPlaceholderText("x 좌표")
+        self.right_y_input.setPlaceholderText("y 좌표")
         self.bottom_right_button = QPushButton('우하단 좌표 설정', self)
-        self.bottom_right_button.clicked.connect(self.set_bottom_right_coordinates)
+        self.bottom_right_button.clicked.connect(self.set_right_bottom_coordinates)
 
-        # 상하좌우 버튼 라디오 버튼
+        # 다음 페이지 방향키 라디오 버튼
+        self.next_button_label = QLabel('다음 페이지 방향키:')
         self.up_radio = QRadioButton('위', self)
         self.down_radio = QRadioButton('아래', self)
         self.left_radio = QRadioButton('왼쪽', self)
@@ -63,17 +73,22 @@ class ScreenshotApp(QWidget):
         # 좌상단 좌표 설정 레이아웃
         top_left_layout = QHBoxLayout()
         top_left_layout.addWidget(self.top_left_label)
+        top_left_layout.addWidget(self.left_x_input)
+        top_left_layout.addWidget(self.left_y_input)
         top_left_layout.addWidget(self.top_left_button)
         layout.addLayout(top_left_layout)
 
         # 우하단 좌표 설정 레이아웃
         bottom_right_layout = QHBoxLayout()
         bottom_right_layout.addWidget(self.bottom_right_label)
+        bottom_right_layout.addWidget(self.right_x_input)
+        bottom_right_layout.addWidget(self.right_y_input)
         bottom_right_layout.addWidget(self.bottom_right_button)
         layout.addLayout(bottom_right_layout)
 
         # 라디오 버튼 레이아웃
         button_layout = QHBoxLayout()
+        button_layout.addWidget(self.next_button_label)
         button_layout.addWidget(self.up_radio)
         button_layout.addWidget(self.down_radio)
         button_layout.addWidget(self.left_radio)
@@ -97,27 +112,44 @@ class ScreenshotApp(QWidget):
 
         self.setLayout(layout)
         self.setWindowTitle('Screenshot convert to PDF')
-        self.setGeometry(100, 100, 400, 250)
+        self.center()
 
-    def set_top_left_coordinates(self):
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def set_left_top_coordinates(self):
         QMessageBox.information(self, '알림', '좌상단 좌표를 설정하려면 브라우저에서 원하는 위치로 이동하세요.')
         time.sleep(self.user_delay)  # 사용자에게 이동 시간을 주기 위해 대기
         self.top_left_x, self.top_left_y = pyautogui.position()
+        self.left_x_input.setText(str(self.top_left_x))
+        self.left_y_input.setText(str(self.top_left_y))
         QMessageBox.information(self, '좌표 설정 완료', f'좌상단 좌표가 ({self.top_left_x}, {self.top_left_y})로 설정되었습니다.')
-        print(f"Top left coordinates set to: ({self.top_left_x}, {self.top_left_y})")
+        print(f"x, y coordinates set to: ({self.top_left_x}, {self.top_left_y})")
 
-    def set_bottom_right_coordinates(self):
+    def set_right_bottom_coordinates(self):
         QMessageBox.information(self, '알림', '우하단 좌표를 설정하려면 브라우저에서 원하는 위치로 이동하세요.')
         time.sleep(self.user_delay)  # 사용자에게 이동 시간을 주기 위해 대기
         self.bottom_right_x, self.bottom_right_y = pyautogui.position()
+        self.right_x_input.setText(str(self.bottom_right_x))
+        self.right_y_input.setText(str(self.bottom_right_y))
         QMessageBox.information(self, '좌표 설정 완료', f'우하단 좌표가 ({self.bottom_right_x}, {self.bottom_right_y})로 설정되었습니다.')
-        print(f"Bottom right coordinates set to: ({self.bottom_right_x}, {self.bottom_right_y})")
+        print(f"x, y coordinates set to: ({self.bottom_right_x}, {self.bottom_right_y})")
 
     def run_screenshot_to_pdf(self):
         # 좌표 및 PDF 파일명 확인
-        if (self.top_left_x, self.top_left_y) == (0, 0) or (self.bottom_right_x, self.bottom_right_y) == (0, 0):
+        if (not self.left_x_input.text().strip() or not self.left_y_input.text().strip() or not self.right_x_input.text().strip() or not self.right_y_input.text().strip()):
             QMessageBox.warning(self, '입력 오류', '좌상단 및 우하단 좌표를 설정하세요.')
             return
+
+        # 다음 페이지 버튼 설정
+        selected_button = self.radio_group.checkedButton()
+        if selected_button is None:
+            QMessageBox.warning(self, '입력 오류', '방향 버튼을 선택하세요.')
+            return
+        button_to_click = selected_button.text()
 
         pdf_filename = self.pdf_input.text()
         if not pdf_filename:
@@ -125,13 +157,6 @@ class ScreenshotApp(QWidget):
             return
         if not pdf_filename.endswith('.pdf'):
             pdf_filename += '.pdf'
-
-        # 클릭할 버튼 설정
-        selected_button = self.radio_group.checkedButton()
-        if selected_button is None:
-            QMessageBox.warning(self, '입력 오류', '방향 버튼을 선택하세요.')
-            return
-        button_to_click = selected_button.text()
 
         # 총 페이지 수 확인
         try:
@@ -142,7 +167,7 @@ class ScreenshotApp(QWidget):
 
         # 스크린샷 및 PDF 변환 실행
         self.current_page = 0
-        self.take_screenshot_and_convert_to_pdf(self.top_left_x, self.top_left_y, self.bottom_right_x, self.bottom_right_y, button_to_click, pdf_filename)
+        self.take_screenshot_and_convert_to_pdf(self.left_x_input.text(), self.left_y_input.text(), self.right_x_input.text(), self.right_y_input.text(), button_to_click, pdf_filename)
 
     def take_screenshot_and_convert_to_pdf(self, top_left_x, top_left_y, bottom_right_x, bottom_right_y, button, pdf_filename):
         # 카운트다운 추가
@@ -158,7 +183,13 @@ class ScreenshotApp(QWidget):
             self.current_page += 1
 
             # 스크린샷 파일명 설정
-            screenshot_filename = f'screenshot_{self.current_page}.png'
+            screenshot_filename = f'image_{self.current_page}.png'
+
+            # 좌표 값을 정수로 변환
+            top_left_x = int(top_left_x)
+            top_left_y = int(top_left_y)
+            bottom_right_x = int(bottom_right_x)
+            bottom_right_y = int(bottom_right_y)
 
             # 스크린샷 찍기
             screenshot_region = (top_left_x, top_left_y, bottom_right_x - top_left_x, bottom_right_y - top_left_y)
@@ -196,6 +227,6 @@ class ScreenshotApp(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = ScreenshotApp()
+    ex = ConvertPdfApp()
     ex.show()
     sys.exit(app.exec_())
